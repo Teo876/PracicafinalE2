53249d603f1d4569ba0c05a9b35f7932d26dc899
Teo876 <20211845@itla.edu.do>
2025-04-17 10:10:59 -0400

    Subiendo cambios

--- test/unit.test.js
+++ test/unit.test.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 
 test('La página contiene "Esta es la práctica final de Electiva 2"', () => {
-  const htmlContent = fs.readFileSync('src/index.html', 'utf8');
+  const htmlContent = fs.readFileSync('index.html', 'utf8');
   expect(htmlContent).toContain('Esta es la práctica final de Electiva 2');
 });
 
